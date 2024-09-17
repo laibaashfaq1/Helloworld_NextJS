341a39198eab001d22c64289c22674015039d30a
@LaibaAshfaq2 <laibaashfaqahmed232@gmail.com>
2024-09-17 19:38:55 +0500

    push my hello world to github

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    /*
+    <>
+    <h1> Hello World</h1>
+    </>
+    yahan pr <> likhna ka bad jo bhi likhain ga error nahi aya ga 
+    </> */
+   <h1>Hello World!</h1>
+  );
+}
+
+//Pascal Case
+//return single element
